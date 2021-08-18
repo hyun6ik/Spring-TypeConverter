5d35a8b549722e5c464d64163faf6b0d1b342251
hyun6ik <fkanwm@naver.com>
2021-08-18 21:54:25 +0900

    Initial commit

---
+++ src/main/java/com/example/spring_typeconverter/SpringTypeconverterApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_typeconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTypeconverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTypeconverterApplication.class, args);
+    }
+
+}
