0193b917266e75165c6d2f373c1b28840b673114
hyun6ik <fkanwm@naver.com>
2021-08-20 00:35:04 +0900

    포맷터 적용하기

--- src/main/java/com/example/spring_typeconverter/WebConfig.java
+++ src/main/java/com/example/spring_typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import com.example.spring_typeconverter.converter.IntegerToStringConverter;
 import com.example.spring_typeconverter.converter.IpPortToStringConverter;
 import com.example.spring_typeconverter.converter.StringToIntegerConverter;
 import com.example.spring_typeconverter.converter.StringToIpPortConverter;
+import com.example.spring_typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,11 +12,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+
+
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+        //주석 처리 우선순위위
+//       registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        //추가
+        registry.addFormatter(new MyNumberFormatter());
+
     }
 }
